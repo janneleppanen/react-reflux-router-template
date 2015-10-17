79ef7c384b49988c8e33d48833ebd782e7b843bd
Janne Leppänen <janne@janneleppanen.com>
2015-10-17 10:42:41 +0300

    Initial commit

---
+++ js/components/child-box.js
@@ -0,0 +1,9 @@
+var ChildBox = React.createClass({
+	render: function() {
+		return (
+			<div>Hi, I am just a child box.</div>
+		);
+	}
+});
+
+module.exports = ChildBox;
---
+++ js/components/main.js
@@ -0,0 +1,17 @@
+var ChildBox = require('./child-box');
+
+var BoxContainer = React.createClass({
+  render: function() {
+    return (
+      <div className="box">
+        Hello, world! I am a Box.
+        <ChildBox />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <BoxContainer />,
+  document.getElementById('content')
+);
