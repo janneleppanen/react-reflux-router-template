6dbc3836bf46e18f7dd4514aa45b7abe2fbaa4fc
Janne Leppänen <janne@janneleppanen.com>
2015-10-18 09:34:16 +0300

    Add React via Bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp        = require('gulp');
 var sourcemaps  = require('gulp-sourcemaps');
+var addsrc      = require('gulp-add-src');
+var concat      = require('gulp-concat');
 var browserify  = require('browserify');
 var babelify    = require('babelify');
 var source      = require('vinyl-source-stream');
@@ -10,23 +12,31 @@ var paths = {
         files:        'js/**/*.js',
         main:         'js/components/main.js',
         compileTo:    'dist',
-        compiledFile: 'app.build.js'
+        compiledFile: 'app.build.js',
+        src:    [
+            'bower_components/react/react.js',
+            'bower_components/react/react-dom.js'
+        ]
     }
 };
 
 gulp.task('scripts', function() {
-    return browserify('./js/components/main')
-    .transform(babelify)
-    .bundle()
-    .on('error', function (err) {
-        console.log(err.toString());
-        this.emit("end");
-    })
-    .pipe(source(paths.js.compiledFile))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.js.compileTo));
+    var bundleStream = browserify(paths.js.main)
+        .transform(babelify)
+        .bundle()
+        .on('error', function (err) {
+            console.log(err.toString());
+            this.emit("end");
+        })
+        .pipe(source('bundled.js'))
+        .pipe(buffer());  
+
+    return bundleStream
+        .pipe(addsrc.prepend(paths.js.src))
+        .pipe(concat(paths.js.compiledFile))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(paths.js.compileTo));
 });
 
 gulp.task('watch', function() {
